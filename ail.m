4c12c63efb3bfec7d0435ca922b34d3376e13ce0
AlexeyGoryunovnb <165417492+AlexeyGoryunovnb@users.noreply.github.com>
2025-03-22 18:51:36 +0300

    Add files via upload

---
+++ ail.m
@@ -0,0 +1,23 @@
+clc; close; clear;
+
+training_set_inputs = [0 0 1; 1 1 1; 1 0 1; 0 1 1];
+training_set_outputs = [0;1;1;0];
+for seed = 1:4
+    rng(seed); synaptic_weights = 2*rand(3,1) - 1;
+    sqrer = zeros(1,1000);
+    k = 1:1000;
+    for i = 1:1000
+        output = 1./(1 + exp(-training_set_inputs * synaptic_weights));
+        sqrer(1,i) = mean((1./(1 + exp(-training_set_inputs * synaptic_weights)) - training_set_outputs).^2);
+        synaptic_weights = synaptic_weights + training_set_inputs' * ((training_set_outputs - output) .* output .* (1 - output));
+    end
+    subplot(2,2,seed)
+    result = 1 / (1 + exp(-[1 1 0]*synaptic_weights))
+    disp(synaptic_weights)
+    plot(k,sqrer);
+    grid on; axis([0 100 0 0.4]);
+end
+
+function R = A(Y)
+    R = [Y(9:32), bitxor(Y(1:8), Y(9:16))];
+end
